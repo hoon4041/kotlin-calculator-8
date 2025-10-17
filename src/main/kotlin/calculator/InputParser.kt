114cf5eb907538a8525f7fc714863fc2c38ad6c8
hoon4041 <simjaehoon3@naver.com>
2025-10-17 16:42:41 +0900

    refactor: add valid process in inputparser, remove validater

--- src/main/kotlin/calculator/InputParser.kt
+++ src/main/kotlin/calculator/InputParser.kt
@@ -2,21 +2,26 @@ package calculator
 
 class InputParser {
 
-    val validater = Validater()
-
     fun parse(input: String): List<Int> {
-        val isValid = validater.validate(input)
-        if(!isValid) { throw IllegalArgumentException("Invalid input")}
-
-        if (input[0].isDigit()) {  // 커스텀 구분자 없이 바로 숫자가 나오는 경우
-            val parts = input.split(Regex("[,:]"))
-            return parts.map { it.trim().toInt() }
-        } else if (input.startsWith("/") && input[1] == '/' && input[3] == '\\' && input[4] == 'n') { // 커스텀 구분자가 있는 경우
+        if(input.isEmpty()) {
+            return emptyList()
+        }
+        if (input.startsWith("//") && input.length > 4 && input[3] == '\\' && input[4] == 'n') { // 커스텀 구분자가 있는 경우
             val customSeparator = input[2]
             val subInput = input.substring(5)
             val parts = subInput.split(Regex("[,:${customSeparator}]"))
-            return parts.map { it.trim().toInt() }
+            try {
+                return parts.map { it.trim().toInt() }
+            } catch (e: NumberFormatException) {
+                throw IllegalArgumentException("Invalid input")
+            }
         }
-        throw IllegalArgumentException("something wrong, warning!!!")
+        // 커스텀 구분자 없는 경우
+        val parts = input.split(Regex("[,:]"))
+        try {
+            return parts.map {it.trim().toInt()}
+        } catch (e: NumberFormatException) { // 숫자가 아닌 다른 값이 입력으로 있을 경우 예외 발생
+            throw IllegalArgumentException("Invalid input")
+        }
     }
 }
