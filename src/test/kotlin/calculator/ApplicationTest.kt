e8f37388b617b772a0189e3d476c41a27adcbc69
hoon4041 <simjaehoon3@naver.com>
2025-10-17 21:12:35 +0900

    test:add ApplicationTest

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -10,19 +10,52 @@ class ApplicationTest : NsTest() {
     @Test
     fun `커스텀 구분자 사용`() {
         assertSimpleTest {
-            run("//;\\n1")
-            assertThat(output()).contains("결과 : 1")
+            run("//;\\n1;2;3")
+            assertThat(output()).contains("결과 : 6")
         }
     }
 
     @Test
-    fun `예외 테스트`() {
+    fun `음수를 포함해도 정상적으로 덧셈된다`() {
         assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("-1,2,3") }
+            run("-1,2,3")
+            assertThat(output()).contains("결과 : 4")
         }
     }
 
-    override fun runMain() {
+    @Test
+    fun `일반 입력 테스트`() {
+        assertSimpleTest {
+            run("1,2,3")
+            assertThat(output()).contains("결과 : 6")
+        }
+    }
+
+    @Test
+    fun `에러 입력 테스트`() {
+        assertSimpleTest {
+            run("1,,3")
+            assertThat(output()).contains("Invalid input")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자를 특수문자로 지정`() {
+        assertSimpleTest {
+            run("//+\\n1,2+3")
+            assertThat(output()).contains("결과 : 6")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 형식 틀릴 경우 예외 발생`() {
+        assertSimpleTest {
+            run("//\n1,3")
+            assertThat(output()).contains("Invalid input")
+        }
+    }
+
+        override fun runMain() {
         main()
     }
 }
