35594608f4ff77ede033b3c88eac418bb08c5b84
hoon4041 <simjaehoon3@naver.com>
2025-10-19 18:01:39 +0900

    feat(InputParser):add valid not allow negative number

--- src/main/kotlin/calculator/InputParser.kt
+++ src/main/kotlin/calculator/InputParser.kt
@@ -6,23 +6,36 @@ class InputParser {
         if(input.isEmpty()) {
             return emptyList()
         }
+
+        val subInput: String
+        val regexPattern: String
+
         if (input.startsWith("//") && input.length > 4 && input[3] == '\\' && input[4] == 'n') { // 커스텀 구분자가 있는 경우
             val customSeparator = input[2]
-            val escapedSeparator = Regex.escape(customSeparator.toString()) // 이게 있어야 특수문자 입력 가능
-            val subInput = input.substring(5)
-            val parts = subInput.split(Regex("[,:]|${escapedSeparator}"))
-            try {
-                return parts.map { it.trim().toInt() }
-            } catch (e: NumberFormatException) {
-                throw IllegalArgumentException("Invalid input")
+
+            if(customSeparator == '-' || customSeparator.isDigit()) {
+                throw IllegalArgumentException("Invalid custom separator(can't use '-' or number")
             }
+            val escapedSeparator = Regex.escape(customSeparator.toString()) // 이게 있어야 특수문자 입력 가능
+            subInput = input.substring(5)
+            regexPattern = "[,:]|$escapedSeparator"
+        } else { // 커스텀 구분자가 없는 경우
+            subInput = input
+            regexPattern = "[,:]"
         }
-        // 커스텀 구분자 없는 경우
-        val parts = input.split(Regex("[,:]"))
-        try {
-            return parts.map {it.trim().toInt()}
-        } catch (e: NumberFormatException) { // 숫자가 아닌 다른 값이 입력으로 있을 경우 예외 발생
+        val parts = subInput.split(Regex(regexPattern))
+
+        val numbers = try {
+            parts.map { it.trim().toInt() }
+        } catch (e: NumberFormatException) {
             throw IllegalArgumentException("Invalid input")
         }
+        // 음수 검증 로직 추가
+        val negativeNumbers = numbers.filter { it < 0 }
+        if (negativeNumbers.isNotEmpty()) {
+            throw IllegalArgumentException("Negative numbers are not supported")
+        }
+
+        return numbers
     }
 }
